b24104c43c657d22fd0bf2f2a165563ed0acc551
Juan Sebastián Mendoza <cebo_052000@hotmail.com>
2023-05-15 16:04:14 -0500

    update .env.example and apollo server config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { ApolloServer } from '@apollo/server';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { resolvers } from 'schema/tasks/resolvers';
 import { typeDefs } from 'schema/tasks/typeDefs';
@@ -6,6 +10,17 @@ import { typeDefs } from 'schema/tasks/typeDefs';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  apollo: {
+    key: process.env.APOLLO_KEY,
+  },
+  plugins: [
+    process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageProductionDefault({
+          embed: true,
+          graphRef: process.env.APOLLO_GRAPH_REF ?? '',
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+  ],
 });
 
 const { url } = await startStandaloneServer(server);
