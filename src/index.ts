95f3f11c5717b8009aa06bfafc5f0bc6306d4c9e
Juan Sebastián Mendoza <cebo_052000@hotmail.com>
2023-06-15 17:53:55 -0500

    Revert "add schema reporting plugin"
    This reverts commit d52ec8b176e34eadc541d16417f1844d2a83bd92.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,25 @@
-import { ApolloServer, ApolloServerPlugin, BaseContext } from '@apollo/server';
+import { ApolloServer, BaseContext } from '@apollo/server';
 import {
   ApolloServerPluginLandingPageLocalDefault,
   ApolloServerPluginLandingPageProductionDefault,
 } from '@apollo/server/plugin/landingPage/default';
-import { ApolloServerPluginSchemaReporting } from '@apollo/server/plugin/schemaReporting';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 import { resolvers } from 'tasks/resolvers';
 
 const typeDefs = readFileSync('src/graphql/tasks/typeDefs.graphql', 'utf-8');
 
-const plugins: ApolloServerPlugin[] = [ApolloServerPluginSchemaReporting()];
-
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(
-    ApolloServerPluginLandingPageProductionDefault({
-      embed: true,
-      graphRef: process.env.APOLLO_GRAPH_REF ?? '',
-    })
-  );
-} else {
-  ApolloServerPluginLandingPageLocalDefault({ embed: true });
-}
-
 const server = new ApolloServer<BaseContext>({
   typeDefs,
   resolvers,
-  plugins,
+  plugins: [
+    process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageProductionDefault({
+          embed: true,
+          graphRef: process.env.APOLLO_GRAPH_REF ?? '',
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+  ],
 });
 
 const { url } = await startStandaloneServer(server);
